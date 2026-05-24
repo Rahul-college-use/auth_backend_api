9810e2cd6f1f2d944d6269aa202af1adc70a902a
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-24 22:27:56 +0530

    cors2

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://auth-frontend-alpha-one.vercel.app" || "https://auth-frontend-alpha-one.vercel.app/" ,
+  origin: "*" ,
   credentials: true,
 };
 
