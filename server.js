e103e46ea98c31f81cad435c29146f2e3f6b14b4
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-25 00:03:58 +0530

    cors1222

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import connectDB from './src/config/database.js';
 
 connectDB();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port : 3000")
-})
+// app.listen(3000,()=>{
+//     console.log("Server is running on port : 3000")
+// })
+export default app;
