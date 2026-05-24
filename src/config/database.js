72c185d298045d98d886e6cf270908b7d477b305
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-24 23:57:13 +0530

    cors1211

--- src/config/database.js
+++ src/config/database.js
@@ -3,33 +3,36 @@ import mongoose from 'mongoose';
 
 let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
 
 const connectDB = async()=>{
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise){
-        const opts={
-            bufferCommands: false,
-        };
-        cached.promise = mongoose.connect(config.MONGO_URL, opts).then((mongoose)=>{
-            console.log("Connected to MongoDB");
-            return mongoose;
-        })
-        try{
-            cached.conn = await cached.promise;
+    // if (cached.conn) {
+    //     return cached.conn;
+    // }
+    // if (!cached.promise){
+    //     const opts={
+    //         bufferCommands: false,
+    //     };
+    //     cached.promise = mongoose.connect(config.MONGO_URL, opts).then((mongoose)=>{
+    //         console.log("Connected to MongoDB");
+    //         return mongoose;
+    //     })
+    //     try{
+    //         cached.conn = await cached.promise;
 
-        }catch(err){
-            cached.promise = null;
-            console.log("Error connecting to MongoDB", err);
-            throw err;
-        }
-        return cached.conn;
-    }
-//    await mongoose.connect(config.MONGO_URL)
-//     console.log("Connected to MongoDB")
+    //     }catch(err){
+    //         cached.promise = null;
+    //         console.log("Error connecting to MongoDB", err);
+    //         throw err;
+    //     }
+    //     return cached.conn;
+    // }
+   await mongoose.connect(config.MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+   })
+    console.log("Connected to MongoDB")
 }
 export default connectDB;
