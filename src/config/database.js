b22411577f6b11c9619f8e1d8d717435a7d955b1
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-24 23:50:16 +0530

    1122

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,28 @@
 import config from './config.js';
 import mongoose from 'mongoose';
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async()=>{
-   await mongoose.connect(config.MONGO_URL)
-    console.log("Connected to MongoDB")
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise){
+        const opts={
+            bufferCommands: false,
+        };
+        cached.promise = mongoose.connect(config.MONGO_URL, opts).then((mongoose)=>{
+            return mongoose;
+        }).catch((err)=>{
+            cached.promise = null;
+            throw err;
+        });
+    }
+//    await mongoose.connect(config.MONGO_URL)
+//     console.log("Connected to MongoDB")
 }
 export default connectDB;
