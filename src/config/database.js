d2d15e740a13f7ffa891792cc081ff03d07bfbe0
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-25 15:26:58 +0530

    c11

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,36 @@
-import config from './config.js';
-import mongoose from 'mongoose';
+// import config from './config.js';
+// import mongoose from 'mongoose';
 
 
-const connectDB = async()=>{
+// const connectDB = async()=>{
  
-   await mongoose.connect(config.MONGO_URL)
-    console.log("Connected to MongoDB")
-}
+//    await mongoose.connect(config.MONGO_URL)
+//     console.log("Connected to MongoDB")
+// }
+// export default connectDB;
+
+
+import mongoose from 'mongoose';
+import config from './config.js';
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log('Using existing database connection');
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(config.MONGO_URL, {
+      bufferCommands: false,
+    });
+    isConnected = db.connections[0].readyState === 1;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Database connection error:', error);
+    throw error;
+  }
+};
+
 export default connectDB;
